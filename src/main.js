aba890858640bb0a0eebfc379745ce46599dac2a
Ewerton Filipe <xewertonfilipe@gmail.com>
2021-02-07 17:33:13 -0300

    google-places

--- src/main.js
+++ src/main.js
@@ -4,15 +4,16 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import VueGeolocation from "vue-browser-geolocation";
-import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 Vue.use(VueGeolocation);
+import * as VueGoogleMaps from "vue2-google-maps";
 Vue.use(VueGoogleMaps, {
   load: {
     key: "AIzaSyDJVZPOwzIn--aX6FRwqTB_V_L1O6Skb-Q",
+    libraries: "places",
   },
   installComponents: true,
 });

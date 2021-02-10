b33ebb77edf74eb748f6a7bc4b6950e33ee7baa6
Ewerton Filipe <xewertonfilipe@gmail.com>
2021-02-10 00:38:30 -0300

    feature/profile

--- src/components/Form/FormLogin/FormLogin.vue
+++ src/components/Form/FormLogin/FormLogin.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="container">
+  <b-container>
     <b-form @submit.stop.prevent="loginSubmit" v-if="show">
-      <b-form-group
-        id="login-email"
-      >
+      <b-form-group id="login-email">
         <b-form-input
           id="input-login-email"
           v-model="$v.form.email.$model"
@@ -12,13 +10,9 @@
           placeholder="E-mail"
           aria-describedby="required-email"
         ></b-form-input>
-
         <b-form-invalid-feedback id="required-email">Obrigatório.</b-form-invalid-feedback>
       </b-form-group>
-
-      <b-form-group 
-        id="login-pass"
-      >
+      <b-form-group id="login-pass">
         <b-form-input
           id="input-login-pass"
           v-model="$v.form.pass.$model"
@@ -27,20 +21,14 @@
           placeholder="Senha"
           aria-describedby="required-pass"
         ></b-form-input>
-
         <b-form-invalid-feedback id="required-pass">Obrigatório.</b-form-invalid-feedback>
       </b-form-group>
-
-      <b-button type="submit" pill>Entrar</b-button>
-      
+      <b-button type="submit">Entrar</b-button>
       <router-link to="/cadastro">
-        <b-button variant="success" pill>Criar Conta</b-button>
+        <b-button variant="success">Criar Conta</b-button>
       </router-link>
     </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
-  </div>
+  </b-container>
 </template>
 
 <script>
